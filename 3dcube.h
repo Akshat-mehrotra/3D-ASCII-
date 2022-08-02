a476756c49de9cfab25d104f98c081f93a4672b6
akshat <akshatmehrotra2004@gmail.com>
2022-08-02 17:14:53 +0530

    updated

--- 3dcube.h
+++ 3dcube.h
@@ -6,17 +6,19 @@
 #include <math.h>
 
 
-#define PLANE 10
-#define DISTANCE 40
-#define res 40
-
+#define MAGNIFICATION 20
+#define DISTANCE 30
+#define res 50
+#define PERSPECTIVE false
 
 using namespace std;
 
 typedef vector<double> t_point;
 typedef vector<t_point> t_vertexs;
 
-void reduce_project(t_point& );
+
+
+void perpspective_projection(t_point& );
 void rotate(t_point&, double);
 void reset_canvas(char**);
 void print_canvas(char**);
